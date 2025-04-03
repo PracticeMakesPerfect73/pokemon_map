c24d507055ea353832bd33efb726e8d6b75a1761
Alexey <practicemakesperfect73@gmail.com>
2025-04-03 23:18:36 +0900

    Отредактировал по PEP8

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,34 +1,77 @@
 from django.db import models  # noqa F401
-from django.utils.timezone import datetime
+
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True, verbose_name='Название')
-    image = models.ImageField(upload_to='pokemons_images/', blank=True, verbose_name='Изображение')
-    description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    title_en = models.CharField(max_length=255, blank=True, null=True, verbose_name='Английское название') 
-    title_jp = models.CharField(max_length=255, blank=True, null=True, verbose_name='Японское название')
+    title = models.CharField(
+        max_length=200, blank=True,
+        verbose_name='Название'
+        )
+    image = models.ImageField(
+        upload_to='pokemons_images/', blank=True,
+        verbose_name='Изображение'
+        )
+    description = models.TextField(
+        blank=True, null=True,
+        verbose_name='Описание'
+        )
+    title_en = models.CharField(
+        max_length=255, blank=True, null=True,
+        verbose_name='Английское название'
+        )
+    title_jp = models.CharField(
+        max_length=255, blank=True, null=True,
+        verbose_name='Японское название'
+        )
     previous_evolution = models.ForeignKey(
-        'self', on_delete=models.SET_NULL, blank=True, null=True, related_name='next_evolutions', verbose_name='Из кого эволюционировал'
+        'self', on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='next_evolutions', verbose_name='Из кого эволюционировал'
     )
-
 
     def __str__(self):
         return f'{self.title}'
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities', verbose_name='Покемон')
-    latitude = models.FloatField(blank=True, null=True, verbose_name='Широта')
-    longitude = models.FloatField(blank=True, null=True, verbose_name='Долгота')
-    appeared_at = models.DateTimeField( blank=True, null=True, verbose_name='Когда появится')
-    disappeared_at = models.DateTimeField( blank=True, null=True, verbose_name='Когда исчезнет')
-
-    level = models.IntegerField(default=1, verbose_name='Уровень')
-    health = models.IntegerField(blank=True, null=True, verbose_name='Количество жизни')
-    strength = models.IntegerField(blank=True, null=True, verbose_name='Сила')
-    defence = models.IntegerField(blank=True, null=True, verbose_name='Защита')
-    stamina = models.IntegerField(blank=True, null=True, verbose_name='Выносливость')
-
+    pokemon = models.ForeignKey(
+        Pokemon, on_delete=models.CASCADE,
+        related_name='entities', verbose_name='Покемон'
+        )
+    latitude = models.FloatField(
+        blank=True, null=True,
+        verbose_name='Широта'
+        )
+    longitude = models.FloatField(
+        blank=True, null=True,
+        verbose_name='Долгота'
+        )
+    appeared_at = models.DateTimeField(
+        blank=True, null=True,
+        verbose_name='Когда появится'
+        )
+    disappeared_at = models.DateTimeField(
+        blank=True, null=True,
+        verbose_name='Когда исчезнет'
+        )
+    level = models.IntegerField(
+        default=1,
+        verbose_name='Уровень'
+        )
+    health = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Количество жизни'
+        )
+    strength = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Сила'
+        )
+    defence = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Защита'
+        )
+    stamina = models.IntegerField(
+        blank=True, null=True,
+        verbose_name='Выносливость'
+        )
 
     def __str__(self):
         return f'{self.pokemon.title} at {self.latitude}, {self.longitude}'
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,10 +1,9 @@
 import folium
-import json
 
-from django.http import HttpResponseNotFound
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+
 from .models import Pokemon, PokemonEntity
-from django.utils import timezone 
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -16,7 +15,6 @@ DEFAULT_IMAGE_URL = (
 
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
-
     icon = folium.features.CustomIcon(
         image_url,
         icon_size=(50, 50),
@@ -31,7 +29,6 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 def show_all_pokemons(request):
     current_time = timezone.localtime()
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
 
     active_pokemon_entities = PokemonEntity.objects.filter(
@@ -46,22 +43,21 @@ def show_all_pokemons(request):
             folium_map,
             pokemon_entity.latitude,
             pokemon_entity.longitude,
-            request.build_absolute_uri(pokemon.image.url) if pokemon.image else DEFAULT_IMAGE_URL
+            request.build_absolute_uri(pokemon.image.url)
+            if pokemon.image else DEFAULT_IMAGE_URL
         )
-        
 
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
-            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else DEFAULT_IMAGE_URL,
+            'img_url': request.build_absolute_uri(pokemon.image.url)
+            if pokemon.image else DEFAULT_IMAGE_URL,
             'title_ru': pokemon.title,
         })
-
 
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
         'pokemons': pokemons_on_page,
     })
-
 
 
 def show_pokemon(request, pokemon_id):
@@ -81,7 +77,8 @@ def show_pokemon(request, pokemon_id):
             folium_map,
             pokemon_entity.latitude,
             pokemon_entity.longitude,
-            request.build_absolute_uri(requested_pokemon.image.url) if requested_pokemon.image else DEFAULT_IMAGE_URL
+            request.build_absolute_uri(requested_pokemon.image.url)
+            if requested_pokemon.image else DEFAULT_IMAGE_URL
         )
 
     prev_evolution_chars = None
@@ -93,7 +90,7 @@ def show_pokemon(request, pokemon_id):
             'img_url': request.build_absolute_uri(previous_evolution.image.url)
             if previous_evolution.image else DEFAULT_IMAGE_URL,
         }
-    
+
     next_evolution_chars = None
     if requested_pokemon.next_evolution.first():
         next_evolution = requested_pokemon.next_evolution.first()
@@ -104,22 +101,18 @@ def show_pokemon(request, pokemon_id):
             if next_evolution.image else DEFAULT_IMAGE_URL,
         }
 
-
-        
-
-
     pokemon_chars = {
         'pokemon_id': requested_pokemon.id,
         'title_ru': requested_pokemon.title,
         'title_en': requested_pokemon.title_en,
         'title_jp': requested_pokemon.title_jp,
         'description': requested_pokemon.description,
-        'img_url': request.build_absolute_uri(requested_pokemon.image.url) if requested_pokemon.image else DEFAULT_IMAGE_URL,
+        'img_url': request.build_absolute_uri(requested_pokemon.image.url)
+        if requested_pokemon.image else DEFAULT_IMAGE_URL,
         'entities': active_pokemon_entities,
         'previous_evolution': prev_evolution_chars,
         'next_evolution': next_evolution_chars,
     }
-    
 
     return render(request, 'pokemon.html', context={
         'map': folium_map._repr_html_(), 'pokemon': pokemon_chars
